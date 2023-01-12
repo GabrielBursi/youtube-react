dafefd1f6c9c5882bd1c5667525fe9d310f7f10d
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-12 14:45:45 -0300

    NavBar e barra de pesquisa

---
+++ src/utils/constants.ts
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote.js';
+import HomeIcon from '@mui/icons-material/Home.js';
+import CodeIcon from '@mui/icons-material/Code.js';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo.js';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports.js';
+import LiveTvIcon from '@mui/icons-material/LiveTv.js';
+import SchoolIcon from '@mui/icons-material/School.js';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural.js';
+import CheckroomIcon from '@mui/icons-material/Checkroom.js';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq.js';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy.js';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter.js';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode.js';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: HomeIcon, },
+  { name: 'JS Mastery', icon: CodeIcon , },
+  { name: 'Coding', icon: CodeIcon , },
+  { name: 'ReactJS', icon: CodeIcon , },
+  { name: 'NextJS', icon: CodeIcon , },
+  { name: 'Music', icon: MusicNoteIcon  },
+  { name: 'Education', icon: SchoolIcon , },
+  { name: 'Podcast', icon: GraphicEqIcon , },
+  { name: 'Movie', icon: OndemandVideoIcon , },
+  { name: 'Gaming', icon: SportsEsportsIcon , },
+  { name: 'Live', icon: LiveTvIcon , },
+  { name: 'Sport', icon: FitnessCenterIcon , },
+  { name: 'Fashion', icon: CheckroomIcon , },
+  { name: 'Beauty', icon: FaceRetouchingNaturalIcon , },
+  { name: 'Comedy', icon: TheaterComedyIcon , },
+  { name: 'Gym', icon: FitnessCenterIcon , },
+  { name: 'Crypto', icon: DeveloperModeIcon , },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
