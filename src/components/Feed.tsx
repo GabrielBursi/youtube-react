1794cd30c99932e95836cee52ec53758bcf52598
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-12 15:18:19 -0300

    sidebar

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-function Feed() {
+import { Sidebar } from "./";
+
+const Feed = () => {
+    
+
+
     return (
-        <div>
-            
-        </div>
+        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+                <Sidebar/>
+                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+                    Copyright © 2022 JSM Media
+                </Typography>
+            </Box>
+        </Stack>
     );
-}
+};
 
 export default Feed;
